8391e89ff032a05e4d7265fe896fcad291146fd1
aluno <aluno@L1M04.IPIRANGA.FATEC>
2018-10-04 21:42:25 -0300

    Aula 04/10 com cobertura de testes das classes: ConfiguraDB e FabricaDeConexoes

---
+++ sce/src/test/java/com/fatec/sce/TestaConexaoComDB.java
@@ -0,0 +1,131 @@
+package com.fatec.sce;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.fatec.sce.model.ConfiguraDB;
+import com.fatec.sce.model.FabricaDeConexoes;
+public class TestaConexaoComDB {
+	/**
+	 * Objetivo - verificar o comportamento do sistema na conexao ao DB com
+	 * configuracao valida Pré-condição - a configuracao default da fabrica de
+	 * conexoes é valida
+	 */
+	@Test
+	public void quandoConectaComOBancoRetornaOK() {
+		// cenario
+		FabricaDeConexoes fabrica;
+		// acao
+		fabrica = new FabricaDeConexoes();
+		// verificacao
+		assertNotNull(fabrica.getConnection());
+	}
+
+	/**
+	 * Objetivo - verificar o comportamento do sistema na conexao ao DB com senha de
+	 * acesso invalida Pré-condição - a senha cadastrada é "aluno"
+	 */
+	@Test
+	public void quandoConectaComSenhaInvalida_SQLException() {
+		// cenario
+		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String driver = "com.mysql.jdbc.Driver";
+		String usuario = "root";
+		String senha = "44"; // senha errada
+		FabricaDeConexoes fabricaDeConexoes = null;
+		ConfiguraDB configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		try {
+			// acao
+			fabricaDeConexoes.getConnection();
+			fail("deveria falhar");
+		} catch (Exception e) {
+			// verificacao
+			System.out.println(e.getMessage());
+			assertEquals(e.getMessage(),
+					"java.sql.SQLException: Access denied for user 'root'@'localhost' (using password: YES)");
+		}
+	}
+	
+	/**
+	 * Objetivo - verificar o comportamento do sistema na conexao ao DB com usuario de
+	 * acesso invalido Pré-condição - o usuário é "root1"
+	 */
+	
+	@Test
+	public void quandoConectaComUsuarioInvalido_SQLException() {
+		// cenario
+		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String driver = "com.mysql.jdbc.Driver";
+		String usuario = "root1";
+		String senha = "a"; // senha errada
+		FabricaDeConexoes fabricaDeConexoes = null;
+		ConfiguraDB configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		try {
+			// acao
+			fabricaDeConexoes.getConnection();
+			fail("deveria falhar");
+		} catch (Exception e) {
+			// verificacao
+			System.out.println(e.getMessage());
+			assertEquals(e.getMessage(),
+					"java.sql.SQLException: Access denied for user 'root1'@'localhost' (using password: YES)");
+		}
+	}
+	
+	/**
+	 * Objetivo - verificar o comportamento do sistema na conexao ao DB com driver
+	 * invalido Pré-condição - o driver é "Driver"
+	 */
+	
+	@Test
+	public void quandoConectaDriverInvalido_SQLException() {
+		// cenario
+		String url = "jdbc:mysql://localhost:3306/biblioteca";
+		String driver = "com.mysql.jdbc.Driver1";
+		String usuario = "root";
+		String senha = "a"; // senha errada
+		FabricaDeConexoes fabricaDeConexoes = null;
+		ConfiguraDB configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		try {
+			// acao
+			fabricaDeConexoes.getConnection();
+			fail("deveria falhar");
+		} catch (Exception e) {
+			// verificacao
+			System.out.println(e.getMessage());
+			assertEquals(e.getMessage(),
+					"java.lang.ClassNotFoundException: com.mysql.jdbc.Driver1");
+		}
+	}
+	
+	/**
+	 * Objetivo - verificar o comportamento do sistema na conexao ao DB com porta
+	 * invalida Pré-condição - a porta de acesso é "3308"
+	 */
+	
+	@Test
+	public void quandoConectaComURLInvalida_SQLException() {
+		// cenario
+		String url = "jdbc:mysql://localhost:3308/biblioteca";
+		String driver = "com.mysql.jdbc.Driver";
+		String usuario = "root";
+		String senha = ""; 
+		FabricaDeConexoes fabricaDeConexoes = null;
+		ConfiguraDB configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		try {
+			// acao
+			fabricaDeConexoes.getConnection();
+			fail("deveria falhar");
+		} catch (Exception e) {
+			// verificacao
+			System.out.println(e.getMessage());
+			assertEquals(e.getMessage(),
+					"com.mysql.jdbc.exceptions.jdbc4.CommunicationsException: Communications link failure\n" + 
+					"\n" + 
+					"The last packet sent successfully to the server was 0 milliseconds ago. The driver has not received any packets from the server.");
+		}
+	}
+	
+} 
