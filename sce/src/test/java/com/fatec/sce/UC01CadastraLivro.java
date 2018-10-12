f30e200dc2fc4ed25207fb52484742c6bf12695a
aluno <aluno@L1M04.IPIRANGA.FATEC>
2018-10-11 21:39:43 -0300

    All tests implementada e ObtemLivro

--- sce/src/test/java/com/fatec/sce/UC01CadastraLivro.java
+++ sce/src/test/java/com/fatec/sce/UC01CadastraLivro.java
@@ -14,9 +14,7 @@ public class UC01CadastraLivro {
 			// cenario
 			Livro umLivro = new Livro();
 			// acao
-			umLivro.setIsbn("121212");
-			umLivro.setTitulo("Engenharia de Softwar");
-			umLivro.setAutor("Pressman");
+			umLivro = ObtemLivro.comDadosValidos();
 		} catch (RuntimeException e) {
 			// verificacao
 			fail("nao deve falhar");
@@ -27,12 +25,9 @@ public class UC01CadastraLivro {
 	public void CT02cadastrarLivroComISBN_em_branco() {
 		// cenario
 		Livro livro = new Livro();
-
-		livro.setTitulo("Engenharia de Software");
-		livro.setAutor("Pressman");
 		try {
 			// acao
-			livro.setIsbn("");
+			livro = ObtemLivro.comISBNInvalido_branco();
 			fail("deveria lançar uma exceção");
 		} catch (RuntimeException e) {
 			// verificacao
@@ -45,12 +40,9 @@ public class UC01CadastraLivro {
 		// cenario
 				Livro livro = new Livro();
 
-				livro.setTitulo("Engenharia de Software");
-				livro.setAutor("Pressman");
-				
 				try {
 					// acao
-					livro.setIsbn(null);
+					livro = ObtemLivro.comISBNInvalido_nulo();
 					fail("deveria lançar uma exceção");
 				} catch (RuntimeException e) {
 					// verificacao
